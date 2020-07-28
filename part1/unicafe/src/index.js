93b8a5b262c48f565df47ad786240865b78f7eef
Yun Li <liyun.aino@icloud.com>
2020-07-28 16:18:48 +0300

    1.9: unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,6 +10,14 @@ const Button = props => {
 }
 
 const Statistics = props => {
+  if (props.total === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h2>statistics</h2>
