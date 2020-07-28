07a7e5e4c783ce12d345882ca049405495a5d0ee
Yun Li <liyun.aino@icloud.com>
2020-07-28 16:11:28 +0300

    1.8:unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,10 +9,16 @@ const Button = props => {
   return <button onClick={props.handleClick}>{props.text}</button>
 }
 
-const Display = props => {
+const Statistics = props => {
   return (
     <div>
-      {props.text} {props.value}
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {props.average}</p>
+      <p>positive {props.positive}</p>
     </div>
   )
 }
@@ -31,13 +37,14 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <Header text="statistics" />
-      <Display text="good" value={good} />
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
-      <Display text="all" value={total} />
-      <Display text="average" value={average} />
-      <Display text="positive" value={positive} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   )
 }
