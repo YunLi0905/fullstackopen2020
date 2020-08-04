fbe495997f5e918c9f35ea8dd235aab5113e0ec8
Yun Li <liyun.aino@icloud.com>
2020-08-04 18:11:51 +0300

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,16 @@ import Person from "./components/person"
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const handleNameChange = event => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = event => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   const addPerson = event => {
@@ -17,11 +23,13 @@ const App = () => {
       setNewName("")
     } else {
       const nameObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
 
       setPersons(persons.concat(nameObject))
       setNewName("")
+      setNewNumber("")
     }
   }
 
@@ -33,6 +41,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
