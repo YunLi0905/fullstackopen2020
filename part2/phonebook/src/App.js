7d0665cc4399426888e3245010b2e74b98da8dff
Yun Li <liyun.aino@icloud.com>
2020-08-05 22:13:29 +0300

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Persons from "./components/persons"
 import Filter from "./components/filter"
 import PersonForm from "./components/personForm"
 
+import axios from "axios"
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    console.log("effect")
+    axios.get("http://localhost:3001/persons").then(response => {
+      console.log("persons")
+      setPersons(response.data)
+    })
+  }, [])
+  console.log("render", persons.length, "persons")
 
   const handleNameChange = event => {
     event.preventDefault()
