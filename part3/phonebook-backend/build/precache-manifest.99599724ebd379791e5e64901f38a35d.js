e3974f7c960f78f1665ace902dc17d486046985b
Yun Li <liyun.aino@icloud.com>
2020-08-17 13:59:46 +0300

    3.9 phonebook backend step9

---
+++ part3/phonebook-backend/build/precache-manifest.99599724ebd379791e5e64901f38a35d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d490e29bf320c9d041d85188ac55898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ba47eaf04d3c51fb6e",
+    "url": "/static/css/main.4b84a2a0.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ba47eaf04d3c51fb6e",
+    "url": "/static/js/main.0284ebe1.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 app.use(cors())
 
 app.use(express.json())
+app.use(express.static("build"))
 
 morgan.token("person", function(req) {
   console.log(JSON.stringify({ name: req.body.name, number: req.body.number }))
