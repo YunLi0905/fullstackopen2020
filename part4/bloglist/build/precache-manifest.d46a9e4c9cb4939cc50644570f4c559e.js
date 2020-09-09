d1e4383ccac7c2101ea7403fe3b1136409ad89ec
Yun Li <liyun.aino@icloud.com>
2020-09-09 17:10:58 +0300

    5.1&5.2 bloglist frontend step1 and 2

---
+++ part4/bloglist/build/precache-manifest.d46a9e4c9cb4939cc50644570f4c559e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d62eb8f6c4b23c91512236e19da433a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ded7af7fb2dfd28657",
+    "url": "/static/js/2.39a77d24.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.39a77d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b6545e41fee85ecfd7",
+    "url": "/static/js/main.fbe5f029.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
